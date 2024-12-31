e318b53634b4f98d2f65764ec8524e92d293a19e
simpboiz <130843673+simpboiz@users.noreply.github.com>
2024-12-31 19:14:44 +0000

    verfiy6

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -90,10 +90,11 @@ class CarController:
       is_accelerating = interp(actuators.accel, [0, .2], [0, 1])
       boost = start_boost * is_accelerating
       # use communityfeaturestoggle to enable faster start from a stop when not in bumper to bumper traffic
-      if Params().get_bool("dp_toyota_rav4_tss2_tune"):
+      #if Params().get_bool("dp_toyota_rav4_tss2_tune"):
+      if Params().getBool("dp_toyota_rav4_tss2_tune") == 1:
         pid_accel_limits = CarInterface.get_pid_accel_limits(self.CP, CS.out.vEgo, None) # Need to get cruise speed from somewhere
         pcm_accel_cmd = 0 if not (CC.longActive) else clip(actuators.accel + boost, pid_accel_limits[0], pid_accel_limits[1])
-        Params().get_bool("dp_toyota_change5speed") = TRUE
+        Params().putbool("dp_toyota_change5speed") = 1
       else:
         pcm_accel_cmd = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
 
